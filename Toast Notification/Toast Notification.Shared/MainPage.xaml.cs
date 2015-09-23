f6d0bd972751231646da644e5b37b5e1ff9218aa
Mohamed Emam <mohamedemam0@outlook.com>
2015-09-23 02:28:00 +0200

    a

---
+++ Toast Notification/Toast Notification.Shared/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using NotificationsExtensions.ToastContent;
+using Windows.UI.Notifications;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Toast_Notification
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+
+            IToastText02 Trying_Toast = ToastContentFactory.CreateToastText02();
+
+            Trying_Toast.TextHeading.Text = "Hey";
+            
+            Trying_Toast.TextBodyWrap.Text = "That’s The content of the toast";
+            
+            ScheduledToastNotification GiveitTime;
+            GiveitTime = new ScheduledToastNotification(Trying_Toast.GetXml(),
+
+            DateTime.Now.AddSeconds(10));
+
+            GiveitTime.Id = "Any ID";
+
+            ToastNotificationManager.CreateToastNotifier().AddToSchedule(GiveitTime);
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+    }
+}
